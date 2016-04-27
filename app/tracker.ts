4b5572ecc5bc5bfb880fc0df2673c10a007493f2
Phil Tsarik <pilip_tsaryk@epam.com>
2016-04-27 13:07:41 +0200

    Refactor nodejs server wrapper to use classes

---
+++ app/tracker.ts
@@ -0,0 +1,34 @@
+/// <reference path="../typings/main.d.ts" />
+import http = require('http');
+
+export class Tracker {
+  private server: http.Server;
+
+  constructor () {
+    this.server = http.createServer();
+
+    this.server.on('close', function () {
+      console.log('close');
+    });
+
+    this.server.on('request', function (request, response) {
+      console.log('request', request.method, request.socket.address(), request.socket.remoteAddress);
+      response.end('data end');
+    });
+
+    this.server.on('clientError', function (err, socket) {
+      console.log('clientError', err, socket);
+    });
+  }
+
+  start (port: number) {
+    port = port || 8080;
+    this.server.listen(port, function () {
+      console.log('Server listening on: http://localhost:%s', port);
+    });
+  }
+
+  close () {
+    this.server.close();
+  }
+}
