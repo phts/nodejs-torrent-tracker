4d94f06675537df7c4d55761ef6eb8b8e2f05d61
Phil Tsarik <pilip_tsaryk@epam.com>
2016-05-06 16:17:43 +0200

    Refactor inner store of TrackerService
    The object should store data for each info_hash'es.

--- app/tracker-service.ts
+++ app/tracker-service.ts
@@ -2,20 +2,25 @@
 import * as _ from 'lodash';
 
 export class TrackerService {
-  private peers: Object;
+  private torrents: Object;
 
   constructor () {
-    this.peers = {};
+    this.torrents = {};
   }
 
   announce (params) {
-    this.peers[params.peerId] = {
+    var torrent = this.getTorrent(params.infoHash);
+    torrent.peers[params.peerId] = {
       peerId: params.peerId,
       ip: params.ip,
       port: params.port
     };
     return {
-      peers: _.values(this.peers)
+      peers: _.values(this.torrents[params.infoHash].peers)
     };
+  }
+
+  private getTorrent(infoHash) {
+    return this.torrents[infoHash] = this.torrents[infoHash] || {peers: {}};
   }
 }
