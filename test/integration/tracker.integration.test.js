8a6c9504d5e2de518f77484e0f1a712073d554e0
Phil Tsarik <phil.tsarik@gmail.com>
2016-05-31 19:40:59 +0200

    Add integration test

---
+++ test/integration/tracker.integration.test.js
@@ -0,0 +1,54 @@
+'use strict';
+var expect = require('chai').expect;
+var http = require('http');
+var bencode = require('bencode');
+
+describe('Tracker', function () {
+  const PORT = 8787;
+  var Tracker = require('../../release/tracker').default,
+    tracker,
+    url;
+
+  beforeEach(function () {
+    tracker = new Tracker();
+    tracker.start(PORT);
+  });
+
+  afterEach(function () {
+    tracker.close();
+  });
+
+  describe('when send a correct announce `started` request', function () {
+    beforeEach(function () {
+      var query = [
+        'info_hash=%f1%05%5b%b6%16%bb%c6%1a%23%b1%bcux%fe%e9%84%8dk%17M',
+        'peer_id=-TR2840-xs32fiev0thq',
+        'port=51413',
+        'uploaded=0',
+        'downloaded=0',
+        'left=100',
+        'numwant=80',
+        'key=43508b98',
+        'compact=1',
+        'supportcrypto=1',
+        'event=started'
+      ];
+      url = `http://localhost:${PORT}/announce?${query.join('&')}`;
+    });
+
+    it('registers this peer and returns a proper response', function (done) {
+      http.get(url, function (response) {
+        var text = '';
+
+        response.on('data', (chunk) => text += chunk);
+        response.on('end', function () {
+          var data = bencode.decode(text);
+          expect(data.complete).to.not.be.undefined;
+          expect(data.incomplete).to.not.be.undefined;
+          expect(data.peers).to.not.be.undefined;
+          done();
+        });
+      });
+    });
+  });
+});
